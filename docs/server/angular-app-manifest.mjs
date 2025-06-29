ba2cf5b054e198b4270d6be2f97ed436a05ccafc
juliocsalves <juliocsalves@gmail.com>
2025-06-28 22:25:02 -0300

    🔥 build final: implementação de responsividade

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/angular-shinobiverse/',
+  baseHref: '/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/angular-shinobiverse"
+    "route": "/"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1962, hash: 'b77498a01830e627ddc25f839a562f612de467d3ce6a71c0c7de51a411f922cd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1214, hash: '0a4affc66f175d3186eeb1a591087e7fd8e8b15377e39c22c379a79f8e41b4a4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 52526, hash: 'b6417c1fb9d5cf44685d120678c4a8172801f7f0837732588f77a4ec5a49789f', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1941, hash: 'b0ba35c843ea9fdf551703a21d37173405204557f969aedf48124b069ef2e402', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1193, hash: 'aeb8597a3a5a5ddfcbcc446241c1471a8aa1a9a74e625a0c066e52ef4a75b95b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 55691, hash: 'be031946e90e259db061eb251d2e6ddc0182ced2eaa68f690759a14e85638fdf', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-H5HSFTQH.css': {size: 73934, hash: 'FLE4wIn6uqQ', text: () => import('./assets-chunks/styles-H5HSFTQH_css.mjs').then(m => m.default)}
   },
 };
