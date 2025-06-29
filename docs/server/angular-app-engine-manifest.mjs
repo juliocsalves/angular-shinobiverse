90454fc371e0de08bd93ebc9fa02d9a591dc0610
juliocsalves <juliocsalves@gmail.com>
2025-06-29 13:07:50 -0300

    🔥 Corrige bug na build e atualiza arquivos da pasta docs

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: '/angular-shinobiverse',
   supportedLocales: {
   "en-US": ""
 },
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/angular-shinobiverse/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/angular-shinobiverse"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1941, hash: '9a738b45d57911a5fd723e2fa425da13423ffa6d7cb2b410d33e0f145a8acdca', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1193, hash: 'e9751090c285cb19380db0cbcf0ee79df2a73a63e7da6223cec7aac03fe016e4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 56363, hash: '3b50a72e07d416018875adfbc8a3511db351527ca5058630d37e11f257b967ac', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1962, hash: '41e74389bc5fc3fa4f196d819cb30aa19474f3a85604ab3d22ba7fb348aa6653', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1214, hash: '1c14e030438a24e9f145d49079326299488ca7fc98aa6b46512fa2ff87dd44bc', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 56384, hash: '1bf4c1549845d1fb77ed9be4dac2f3a817cac6b38b73fb0dbf238dae426bcda4', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-H5HSFTQH.css': {size: 73934, hash: 'FLE4wIn6uqQ', text: () => import('./assets-chunks/styles-H5HSFTQH_css.mjs').then(m => m.default)}
   },
 };
