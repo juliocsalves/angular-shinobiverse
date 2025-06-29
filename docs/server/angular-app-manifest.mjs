8c0e3e7b02781dd2f0f3473945bb1bac71d9f071
juliocsalves <juliocsalves@gmail.com>
2025-06-29 00:04:16 -0300

    Deploy: build final corrigida com responsividade e assets atualizados

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1962, hash: '54e299135b3d4a9a11e78e99fb2cbb485e084c659a0b78bcc9f410da917c6060', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1214, hash: 'f44414e507ddce72f400f147f9db306e8763383c431cc99edabb2c33bfeeb7f7', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 56021, hash: '43011409d87fc17fab40622975c20eb79e7b52744b59707945649452af96998a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1962, hash: '4b62ec29a6a0aa841500aaf6dc46f2f55a7762c41ebff478810d02e79dad6302', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1214, hash: '621319eb2eb4e4214e0a55bcb535afd662797737ac11fb3f6418cb8e6efd0a21', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 56313, hash: 'b492b9d8418c0d82476a58a95d651b741ebd113ff4a879f5e6c31b18cf85897c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-H5HSFTQH.css': {size: 73934, hash: 'FLE4wIn6uqQ', text: () => import('./assets-chunks/styles-H5HSFTQH_css.mjs').then(m => m.default)}
   },
 };
