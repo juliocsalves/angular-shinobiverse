d7f38d610a0ac10e63bc95e9689f5b6885e3c747
juliocsalves <juliocsalves@gmail.com>
2025-06-28 22:57:30 -0300

    Atualiza build do Angular na pasta docs para deploy

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/angular-shinobiverse/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/angular-shinobiverse"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1941, hash: 'b881097ff8e8afa41aaaacf6b73d8278311ad3d01da8cac7e2264c75e814220f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1193, hash: '87dc3b23fc4d08353d9fba2502214d6d5d7620e54b2954760c02e9397c56688c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 56000, hash: 'e8505ead727c510aeac3707d0a0744ef8df0067c3c92d9d91ff3a2852c124c5d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1962, hash: '54e299135b3d4a9a11e78e99fb2cbb485e084c659a0b78bcc9f410da917c6060', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1214, hash: 'f44414e507ddce72f400f147f9db306e8763383c431cc99edabb2c33bfeeb7f7', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 56021, hash: '43011409d87fc17fab40622975c20eb79e7b52744b59707945649452af96998a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-H5HSFTQH.css': {size: 73934, hash: 'FLE4wIn6uqQ', text: () => import('./assets-chunks/styles-H5HSFTQH_css.mjs').then(m => m.default)}
   },
 };
